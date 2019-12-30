2bb3a53348f0225c5630d7c2654088ff52aa74f3
Marco van de Lindt <marcovandelindt@live.nl>
2019-12-30 10:17:31 +0100

    Setup

---
+++ application/controllers/Home.php
@@ -0,0 +1,41 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        # Load helpers
+        $this->load->helper(
+            [
+                'url',
+            ]
+        );
+
+        # Load libraries
+        $this->load->library(
+            [
+                'session',
+            ]
+        );
+    }
+
+    /**
+     * Index action
+     */
+    public function index()
+    {
+        # Set data
+        $data = [
+            'title' => 'Home',
+        ];
+
+        $this->load->view('layouts/header', $data);
+        $this->load->view('home/index', $data);
+        $this->load->view('layouts/footer', $data);
+    }
+}
---
+++ application/views/layouts/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+    <title><?= (!empty($title) ? $title : '') ?></title>
+
+    <!-- Application stylesheet -->
+    <link rel="stylesheet" type="text/css" href="public/css/app.css" media="screen"/>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/98f93d3825.js" crossorigin="anonymous"></script>
+</head>
+<body>
